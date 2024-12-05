7bba754a6a446a63be0bf8e9ca65e7f65d968e83
wirawahyu <wirawahyu225@gmail.com>
2024-12-06 03:55:31 +0700

    sorting book trip

--- Travel/resources/views/trips/index.blade.php
+++ Travel/resources/views/trips/index.blade.php
@@ -2,37 +2,47 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Trips</h1>
-<a href="{{ route('trips.create') }}" class="btn btn-primary">Add Trip</a>
-<table class="table">
-    <thead>
-        <tr>
-            <th>#</th>
-            <th>Destination</th>
-            <th>Description</th>
-            <th>Price</th>
-            <th>Duration</th>
-            <th>Actions</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($trips as $trip)
-        <tr>
-            <td>{{ (string)$trip['_id'] }}</td>
-            <td>{{ $trip['destination'] }}</td>
-            <td>{{ $trip['description'] }}</td>
-            <td>{{ $trip['price'] }}</td>
-            <td>{{ $trip['duration'] }} days</td>
-            <td>
-                <a href="{{ route('trips.edit', ['trip' => (string)$trip['_id']]) }}" class="btn btn-warning">Edit</a>
-                <form action="{{ route('trips.destroy', ['trip' => (string)$trip['_id']]) }}" method="POST" style="display:inline-block;">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="btn btn-danger">Delete</button>
-                </form>
-            </td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
+<div ng-app="tripApp" ng-controller="tripController">
+    <h1>Trips</h1>
+    <div class="form-group mb-3">
+        <input type="text" class="form-control" ng-model="searchText" placeholder="Search destination...">
+    </div>
+    <a href="{{ route('trips.create') }}" class="btn btn-primary">Add Trip</a>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Destination</th>
+                <th>Description</th>
+                <th>Price</th>
+                <th>Duration</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr ng-repeat="trip in trips | filter:searchText">
+                <td>@{{ trip._id }}</td>
+                <td>@{{ trip.destination }}</td>
+                <td>@{{ trip.description }}</td>
+                <td>@{{ trip.price }}</td>
+                <td>@{{ trip.duration }} days</td>
+                <td>
+                    <a href="/trips/@{{ trip._id }}/edit" class="btn btn-warning">Edit</a>
+                    <form action="/trips/@{{ trip._id }}" method="POST" style="display:inline-block;">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.8.2/angular.min.js"></script>
+<script>
+    var app = angular.module('tripApp', []);
+    app.controller('tripController', function($scope) {
+        $scope.trips = @json($trips);
+    });
+</script>
 @endsection

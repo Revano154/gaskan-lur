c9a8ccb0735d588792fcbcec5b581f7dba288872
Revano154 <revanokahar009@gamil.com>
2024-12-05 20:01:16 +0700

    database register

--- Travel/resources/views/register.blade.php
+++ Travel/resources/views/register.blade.php
@@ -26,6 +26,24 @@
             <button type="submit" class="btn btn-primary w-100">Register</button>
         </form>
     </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="successModal" tabindex="-1" aria-labelledby="successModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="successModalLabel">Registration Successful</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Congratulations! You have successfully created an account.
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
